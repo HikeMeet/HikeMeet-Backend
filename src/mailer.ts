81474f30556f29b813ebeb797033305fe0cac39d
woozai <wooozai@gmail.com>
2025-06-04 17:15:47 +0300

    fix mailer FOR KUBER

--- src/mailer.ts
+++ src/mailer.ts
@@ -2,7 +2,9 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   //"user" and "pass" need move to ENV file
-  service: 'Gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // upgrade with STARTTLS
   auth: {
     user: 'royinagar2@gmail.com',
     pass: 'pryk uqde apyp kuwl',
